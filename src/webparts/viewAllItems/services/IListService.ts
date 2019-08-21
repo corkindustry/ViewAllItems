877dd615189cdc1a9ee7dda1a7dcb317e7233e64
jonah <falsefriend13@gmail.com>
2019-08-21 16:49:00 -0400

    added a semicolon

--- src/webparts/viewAllItems/services/IListService.ts
+++ src/webparts/viewAllItems/services/IListService.ts
@@ -1,5 +1,5 @@
 import { IList } from './IList';
-import { IListColumn } from './IListColumn'
+import { IListColumn } from './IListColumn';
 export interface IListService {
   getLists: () => Promise<IList[]>;
   getColumns: (listName) => Promise<IListColumn[]>;
