7373df314c10bd444a0cf2e7b471aff52815120d
jonah <falsefriend13@gmail.com>
2019-08-21 16:06:03 -0400

    first

---
+++ src/webparts/viewAllItems/loc/mystrings.d.ts
@@ -0,0 +1,12 @@
+declare interface IViewAllItemsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  ListNameFieldLabel: string;
+  ColumnFieldLabel: string;
+  PageSizeFieldLabel: string;
+}
+
+declare module 'ViewAllItemsWebPartStrings' {
+  const strings: IViewAllItemsWebPartStrings;
+  export = strings;
+}
